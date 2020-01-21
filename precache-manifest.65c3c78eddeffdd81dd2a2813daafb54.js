637d4f0bfbb1305763074d44a44b8e11cb03186f
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-01-21 16:50:44 +0000

    Updates

---
+++ precache-manifest.65c3c78eddeffdd81dd2a2813daafb54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d019d1d72f246bd365f4d909eab1422e",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "8b754055709eac2d275a",
+    "url": "/chat-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7073207f5a9606c61e2",
+    "url": "/chat-app/static/js/2.ecea27bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chat-app/static/js/2.ecea27bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b754055709eac2d275a",
+    "url": "/chat-app/static/js/main.35bdfacd.chunk.js"
+  },
+  {
+    "revision": "f477ffcb83b4bd526d63",
+    "url": "/chat-app/static/js/runtime-main.adcae69f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
